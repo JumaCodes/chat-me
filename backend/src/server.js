d94d4abcffbc1bfe59d34eb256f377074b0c24fb
JumaCodes <olumesestevejohn@gmail.com>
2025-09-17 12:29:45 +0100

    trying to deploy to Savella

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,13 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js"; 
-import messageRoutes from "./routes/messageRoutes.js"; 
+import messageRoutes from "./routes/messageRoutes.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
@@ -22,6 +24,15 @@ app.use("/api/messages", messageRoutes);
 // GET /api/auth/signup
 // GET /api/auth/signin
 // GET /api/auth/logout
+
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
 
 // Example route
 app.get("/", (req, res) => {
