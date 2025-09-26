b01a8d2cba54a5dc01c31ca9621cc696c2b68959
JumaCodes <olumesestevejohn@gmail.com>
2025-09-26 13:36:32 +0100

    Setup the chat-app and created it components

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
-import { useAuthStore } from '../zustand/store/useAuthStore';
+// import { useAuthStore } from '../zustand/store/useAuthStore';
+import BoarderAnimatedContainer from "../components/BoarderAnimatedContainer";
+import { useChatStore } from '../zustand/store/useChatStore';
+import ProfileHeader from '../components/ProfileHeader';
+import ActiveTabSwitch from '../components/ActiveTabSwitch';
+import ChatList from '../components/ChatList';
+import ContactList from '../components/ContactList';
+import ChatContainer from '../components/ChatContainer';
+import WelcomeMessage from '../components/WelcomeMessage';
 
 const ChatPage = () => {
-  const { logout } = useAuthStore();
+  // const { logout } = useAuthStore();
+  const { activeTab, selectedUser } = useChatStore();
   return (
-    <div>
-      <div className="min-h-screen flex flex-col items-center justify-center text-white">
-        <h1 className="text-4xl font-bold mb-6">ChatPage</h1>
+    <div className="relative w-full max-w-6xl m-auto h-[800px]">
+      <BoarderAnimatedContainer>
+        {/* LEFT SIDE */}
+        <div className="w-80 bg-slate-800/50 backdrop-blue-sm flex flex-col">
+          <ProfileHeader />
+          <ActiveTabSwitch />
 
-        <button onClick={logout} className="btn btn-primary">Logout</button>
-      </div>
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+            {activeTab === "chats" ? <ChatList /> : <ContactList />}
+          </div>
+        </div>
+
+        {/* RIGHT SIDE */}
+        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blue-sm">
+          {selectedUser ? <ChatContainer /> : <WelcomeMessage />}
+        </div>
+      </BoarderAnimatedContainer>
     </div>
   );
 }
